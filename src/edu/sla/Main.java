ff1a7338c2febdca9328652a825b44ac8b284755
vhdez <vhernandez@scienceleadership.org>
2017-09-20 11:17:12 -0400

    Initial commit

---
+++ src/edu/sla/Main.java
@@ -0,0 +1,10 @@
+package edu.sla;
+
+public class Main {
+
+    public static void main(String[] args) {
+        WishesItWasSomething wish1 = new WishesItWasSomething();
+	    System.out.println("Someday this program will do something.");
+        wish1.saySomething();
+    }
+}
---
+++ src/edu/sla/WishesItWasSomething.java
@@ -0,0 +1,8 @@
+package edu.sla;
+
+public class WishesItWasSomething {
+    void saySomething() {
+        System.out.println("I wish I had something to say.");
+
+    }
+}
